fda5cdf4bdbd39f05df47fdaf04a054c6c74c7c6
TN BusTrack <deploy@tnbustrack.app>
2026-06-24 04:45:00 +0100

    Fix page instability: remove skipWaiting from service worker to prevent mid-session reload

--- public/sw.js
+++ public/sw.js
@@ -5,7 +5,6 @@ self.addEventListener('install', (event) => {
   event.waitUntil(
     caches.open(CACHE)
       .then((cache) => cache.addAll(PRECACHE).catch(() => {}))
-      .then(() => self.skipWaiting())
   );
 });
 
